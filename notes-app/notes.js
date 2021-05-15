8400b5c98a8a21af6bb63355ae7298fb96aeb655
melonsoda20 <benedictusteddy200596@gmail.com>
2021-05-15 22:58:49 +0700

    Add feature to remove data from JSON file

--- notes-app/notes.js
+++ notes-app/notes.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const chalk = require('chalk');
 
 const getNotes = function(){
     return 'Your notes...';
@@ -25,6 +26,21 @@ const addNote = function(title, body){
     }
 }
 
+const removeNote = function(title){
+    const notes = loadNotes();
+    const notesToKeep =  notes.filter(function(note) {
+        return note.title !== title 
+    });
+
+    if(notes.length > notesToKeep.length ){
+        console.log(chalk.green.inverse('Note removed!'));
+        saveNotes(notesToKeep);
+    }
+    else{
+        console.log(chalk.red.inverse('No note found!'));
+    }
+}
+
 const saveNotes = function(notes) {
     const dataJSON = JSON.stringify(notes);
     fs.writeFileSync('notes.json', dataJSON);
@@ -42,5 +58,6 @@ const loadNotes = function() {
 
 module.exports = {
     getNotes: getNotes,
-    addNote: addNote
+    addNote: addNote,
+    removeNote: removeNote
 };
