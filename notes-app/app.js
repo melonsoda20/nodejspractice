c6dfb2d82508c9ca557e640bf44d40938c423c28
melonsoda20 <benedictusteddy200596@gmail.com>
2021-05-15 19:37:24 +0700

    Add yargs to the project

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,14 +1,59 @@
 const chalk = require('chalk');
+const yargs = require('yargs');
 const getNotes = require('./notes');
 
-const msg = getNotes();
-console.log(msg);
+//Customize yargs version
+yargs.version('1.1.0');
 
-const greenMsg = chalk.bold.inverse.green('Success!');
-console.log(greenMsg);
+// Create add command
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            describe: 'Note body',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function (argv) {
+        console.log('Title: ' + argv.title);
+        console.log('Body: ' + argv.body);
+    }
+});
 
-// const add = require('./utils.js');
+// Create remove command
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    handler: function () {
+        console.log('Removing the note');
+    }
+});
 
-// const sum = add(4, -2);
+// Create read command
+yargs.command({
+    command: 'read',
+    describe: 'Read a note',
+    handler: function () {
+        console.log('Reading a note');
+    }
+});
 
-// console.log(sum);
+// Create list command
+yargs.command({
+    command: 'list',
+    describe: 'List your notes',
+    handler: function () {
+        console.log('Listing out all note');
+    }
+});
+
+yargs.parse();
+
+// console.log(yargs.argv);
