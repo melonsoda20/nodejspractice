b0e529266eff84d1954b4402224d815c9c8290d6
melonsoda20 <benedictusteddy200596@gmail.com>
2021-05-16 12:25:41 +0700

    Add feature to read a note

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,5 +1,3 @@
-
-const chalk = require('chalk');
 const yargs = require('yargs');
 const notes = require('./notes');
 
@@ -47,8 +45,15 @@ yargs.command({
 yargs.command({
     command: 'read',
     describe: 'Read a note',
-    handler() {
-        console.log('Reading a note');
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler(argv) {
+        notes.readNote(argv.title);
     }
 });
 
