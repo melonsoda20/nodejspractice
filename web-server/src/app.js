3e2bf940ea2e0fde112f7aed1a360e94026e4244
melonsoda20 <benedictusteddy200596@gmail.com>
2021-05-16 22:10:50 +0700

    Set up hbs templating engine

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -4,16 +4,35 @@ const express = require('express');
 const app = express();
 const publicDirectoryPath = path.join(__dirname, '../public');
 
+app.set('view engine', 'hbs');
 app.use(express.static(publicDirectoryPath));
 
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather',
+        name: 'Benedictus Teddy'
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About Me',
+        name: 'Benedictus Teddy'
+    });
+});
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        helpText: 'This is some helpful text.'
+    });
+});
+
+
 app.get('/weather', (req, res) => {
-    res.send([{
+    res.send({
         forecast: 'It is snowing',
         location: 'Philadelphia'
-    }, {
-        forecast: 'It is raining',
-        location: 'Boston'
-    }]);
+    });
 });
 
 app.listen(3000, () => {
