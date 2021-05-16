b591a22edd638a1ba33cfbf93d2b542d74a463da
melonsoda20 <benedictusteddy200596@gmail.com>
2021-05-16 21:47:12 +0700

    Create static assets for web server

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,24 +1,10 @@
+const path = require('path');
 const express = require('express');
 
 const app = express();
+const publicDirectoryPath = path.join(__dirname, '../public');
 
-app.get('', (req, res) => {
-    res.send('<h1>Weather</h1>');
-});
-
-app.get('/help', (req, res) => {
-    res.send([{
-        name: 'Teddy',
-        age: 24
-    }, {
-        name: 'Sarah',
-        age: 19
-    }]);
-});
-
-app.get('/about', (req, res) => {
-    res.send('<h1>About Page</h1>');
-});
+app.use(express.static(publicDirectoryPath));
 
 app.get('/weather', (req, res) => {
     res.send([{
