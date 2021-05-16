610bec2d9b4bd79152452b36ec748869ac5c4642
melonsoda20 <benedictusteddy200596@gmail.com>
2021-05-16 20:32:33 +0700

    Refactor code to use object destructuring

--- weather-app/utils/geocode.js
+++ weather-app/utils/geocode.js
@@ -3,18 +3,18 @@ const request = require('request');
 const geocode = (address, callback) => {
     const geocodeURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(address) + '.json?access_token=pk.eyJ1IjoibHVtaW51b3VzOTkwIiwiYSI6ImNrb3IxbXg0ZzB2NGEyd3BwMHowMWR1ZHUifQ.xxExrcGRz2hV_F7UkPyq0Q&limit=1';
 
-    request({ url: geocodeURL, json: true}, (error, response) => {
+    request({ url: geocodeURL, json: true}, (error, { body }) => {
         if(error){
             callback('Unable to connect to location services!', undefined);
         }
-        else if(response.body.features.length === 0){
+        else if(body.features.length === 0){
             callback('Unable to find location. Please try another search', undefined);
         }
         else{
             callback(undefined, {
-                latitude: response.body.features[0].center[1],
-                longitude: response.body.features[0].center[0],
-                location: response.body.features[0].place_name
+                latitude: body.features[0].center[1],
+                longitude: body.features[0].center[0],
+                location: body.features[0].place_name
             });
         }
     });
