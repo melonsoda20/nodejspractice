b2d4fe3bda458b649beefca2163155bf85b2d520
melonsoda20 <benedictusteddy200596@gmail.com>
2021-05-17 21:55:29 +0700

    Update templates to display data from weather API

---
+++ web-server/src/utils/forecast.js
@@ -0,0 +1,19 @@
+const request = require('request');
+
+const forecast = (latitude, longitude, callback) => {
+    const forecastURL = 'http://api.weatherstack.com/current?access_key=9984e823c7343dfebeef3f55da2db98a&query=' + latitude + ',' + longitude  + '&units=f';
+
+    request({ url: forecastURL, json: true}, (error, { body }) => {
+        if(error){
+            callback('Unable to connect to weather services!', undefined);
+        }
+        else if(body.error){
+            callback('Unable to find location', undefined);
+        }
+        else{
+            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees out. It feels like ' + body.current.feelslike + ' degrees out.');
+        }
+    });
+};
+
+module.exports = forecast;
