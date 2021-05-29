d4062ea86c71a48cbdbfa9d3bcdb2b44be8f51e9
melonsoda20 <benedictusteddy200596@gmail.com>
2021-05-29 08:54:43 +0700

    Create update method for task manager

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -56,6 +56,30 @@ app.get('/users/:id', async (req, res) => {
     // });
 });
 
+app.patch('/users/:id', async(req, res) => {
+    const updates = Object.keys(req.body);
+    const allowedUpdates = ['name', 'email', 'password', 'age'];
+    const isValidOperation = updates.every((update) => {
+        return allowedUpdates.includes(update);
+    });
+
+    if(!isValidOperation){
+       return res.status(400).send({ error: 'Invalid updates!'}); 
+    }
+
+    try {
+        const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true });
+
+        if(!user){
+            return res.status(404).send();
+        }
+
+        res.send(user);
+    } catch (error) {
+        res.status(400).send(error);
+    }
+});
+
 app.post('/tasks', async (req, res) => {
     const task = new Task(req.body);
     try {
@@ -105,6 +129,30 @@ app.get('/tasks/:id', async (req, res) => {
     // }).catch((e) => {
     //     res.status(404).send();
     // });
+});
+
+app.patch('/tasks/:id', async (req, res) => {
+    const updates = Object.keys(req.body);
+    const allowedUpdates = ['description', 'completed'];
+    const isValidOperation = updates.every((update) => {
+        return allowedUpdates.includes(update)
+    });
+
+    if(!isValidOperation){
+        return res.status(400).send({error: 'Invalud updates!'});
+    }
+
+    try {
+        const task = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true });
+        
+        if(!task){
+            return res.status(404).send();
+        }
+
+        res.send(task);
+    } catch (error) {
+        res.status(400).send(e);
+    }
 });
 
 app.listen(port, () => {
